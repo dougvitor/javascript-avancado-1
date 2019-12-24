d387fb4d25f1c1c60abaa14d9e9957840833dde3
douguees <admin@example.com>
2019-12-24 00:33:12 -0300

    Aula 6 - Herança e reutilização de código, Conclusão da primeira parte do curso

--- client/js/app/controller/NegociacaoController.js
+++ client/js/app/controller/NegociacaoController.js
@@ -11,11 +11,9 @@ class NegociacaoController{
         this._listaNegociacoes = new ListaNegociacoes();
 
         this._negociacoesView = new NegociacoesView($('#negociacoesView'));
-
         this._negociacoesView.update(this._listaNegociacoes);
 
         this._mensagem = new Mensagem();
-
         this._mensagemView = new MensagemView($('#mensagemView'));
         this._mensagemView.update(this._mensagem);
     }
