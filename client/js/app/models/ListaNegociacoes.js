db437d78c67dac23880bf7405208bddd39508a8e
douguees <admin@example.com>
2019-12-25 22:49:59 -0300

    P2 Aula 2 - Criação de Objeto Proxy

--- client/js/app/models/ListaNegociacoes.js
+++ client/js/app/models/ListaNegociacoes.js
@@ -1,14 +1,11 @@
 class ListaNegociacoes{
 
-    constructor(armadilha){
+    constructor(){
         this._negociacoes = [];
-        this._armadilha = armadilha;
     }
 
     add(negociacao){
         this._negociacoes.push(negociacao);
-        this._armadilha(this);
-        //Reflect.apply(this._armadilha, this._contexto, [this]);
     }
 
     get negociacoes(){
@@ -18,7 +15,5 @@ class ListaNegociacoes{
     esvazia(){
 
         this._negociacoes = [];
-        this._armadilha(this);
-        //Reflect.apply(this._armadilha, this._contexto, [this]);
     }
 }
